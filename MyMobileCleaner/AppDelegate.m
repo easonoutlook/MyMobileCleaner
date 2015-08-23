ed082658c23f8b1e2c82ae9b307c98fb17c62280
wzqcongcong <wzqcongcong@sina.com>
2015-08-23 10:22:52 +0800

    delay to show main window, so there's no flash because of "no connection" -> "connected".

--- MyMobileCleaner/AppDelegate.m
+++ MyMobileCleaner/AppDelegate.m
@@ -21,10 +21,16 @@
     // Insert code here to initialize your application
 
     self.mainWindowController = [[MCMainWindowController alloc] init];
-    [self.mainWindowController showWindow:self];
-    [self.mainWindowController.window makeKeyAndOrderFront:self];
+    self.mainWindowController.window.canHide = YES;
 
     [self.mainWindowController goToWork];
+
+    // delay to show main window, so there's no flash because of "no connection" -> "connected".
+    // remember to uncheck "Visible At Launch" in main window's xib
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        [self.mainWindowController showWindow:self];
+        [self.mainWindowController.window makeKeyAndOrderFront:self];
+    });
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
